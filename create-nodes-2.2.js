18044e1fcfa30ebefc289dfe65e73edb3bf8f1c3
Galahad <100938093+Ya-Aladdin@users.noreply.github.com>
2022-04-26 22:29:04 +0300

    Update create-nodes-2.2.js

--- create-nodes-2.2.js
+++ create-nodes-2.2.js
@@ -3,22 +3,23 @@ const array = ["html", "css", "js"];
 
 const render = (array) => {
 
-    const ul = document.createElement("ul")
-    body.append(ul);
+    const ul = document.createElement("ul");
 
     // for (let i = 0; i < array.length; i++) {
-    //     const li = document.createElement("li")
+    //     const li = document.createElement("li");
     //     li.textContent = array[i];
-    //     ul.append(li)
+    //     ul.append(li);
     // }
 
     //Сделал 2-мя способами через цикл for и for of
 
     for (item of array) {
-        const li = document.createElement("li")
+        const li = document.createElement("li");
         li.textContent = item;
-        ul.append(li)
+        ul.append(li);
     }
+    
+    return body.append(ul); 
 }
 
 render(array);
